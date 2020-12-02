fa09501b0f4cc176106317d15af71738361d86a9
Adrien Quillet <adrienquillet@gmail.com>
2020-12-02 23:08:44 +0100

    🐛 debug and new drug

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Drug, Pharmacy } from "./pharmacy";
+import { Drug, Pharmacy } from "./pharmacy.js";
 
 import fs from "fs";
 
@@ -6,7 +6,8 @@ const drugs = [
   new Drug("Doliprane", 20, 30),
   new Drug("Herbal Tea", 10, 5),
   new Drug("Fervex", 5, 40),
-  new Drug("Magic Pill", 15, 40)
+  new Drug("Magic Pill", 15, 40),
+  new Drug("Dafalgan", 15, 40)
 ];
 const trial = new Pharmacy(drugs);
 
@@ -17,7 +18,7 @@ for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
 }
 
 /* eslint-disable no-console */
-fs.writeFile("output.txt", log, err => {
+fs.writeFile("output.txt", log.toString(), err => {
   if (err) {
     console.log("error");
   } else {
