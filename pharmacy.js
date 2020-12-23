9e9717411c97edca020c566093c1900305cf77be
Max Janvier <max.janvier@epitech.eu>
2020-12-23 17:30:33 +0100

    adds Dafalgan drug and tests all cases

--- pharmacy.js
+++ pharmacy.js
@@ -13,49 +13,52 @@ export class Pharmacy {
   updateBenefitValue() {
     for (var i = 0; i < this.drugs.length; i++) {
       if (
-        this.drugs[i].name != "Herbal Tea" &&
-        this.drugs[i].name != "Fervex"
+        this.drugs[i].name !== "Herbal Tea" &&
+        this.drugs[i].name !== "Fervex" &&
+        this.drugs[i].name !== "Magic Pill" &&
+        this.drugs[i].benefit > 0
       ) {
-        if (this.drugs[i].benefit > 0) {
-          if (this.drugs[i].name != "Magic Pill") {
-            this.drugs[i].benefit = this.drugs[i].benefit - 1;
-          }
-        }
+        this.drugs[i].name === "Dafalgan"
+          ? (this.drugs[i].benefit -= 2)
+          : (this.drugs[i].benefit -= 1);
       } else {
-        if (this.drugs[i].benefit < 50) {
-          this.drugs[i].benefit = this.drugs[i].benefit + 1;
+        if (this.drugs[i].benefit < 50 && this.drugs[i].name !== "Magic Pill") {
+          this.drugs[i].name === "Dafalgan"
+            ? (this.drugs[i].benefit += 2)
+            : (this.drugs[i].benefit += 1);
           if (this.drugs[i].name == "Fervex") {
             if (this.drugs[i].expiresIn < 11) {
               if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
+                this.drugs[i].benefit += 1;
               }
             }
             if (this.drugs[i].expiresIn < 6) {
               if (this.drugs[i].benefit < 50) {
-                this.drugs[i].benefit = this.drugs[i].benefit + 1;
+                this.drugs[i].benefit += 1;
               }
             }
           }
         }
       }
-      if (this.drugs[i].name != "Magic Pill") {
-        this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
+      if (this.drugs[i].name !== "Magic Pill") {
+        this.drugs[i].expiresIn -= 1;
       }
       if (this.drugs[i].expiresIn < 0) {
-        if (this.drugs[i].name != "Herbal Tea") {
-          if (this.drugs[i].name != "Fervex") {
+        if (this.drugs[i].name !== "Herbal Tea") {
+          if (this.drugs[i].name !== "Fervex") {
             if (this.drugs[i].benefit > 0) {
-              if (this.drugs[i].name != "Magic Pill") {
-                this.drugs[i].benefit = this.drugs[i].benefit - 1;
+              if (this.drugs[i].name !== "Magic Pill") {
+                this.drugs[i].name === "Dafalgan"
+                  ? (this.drugs[i].benefit -= 2)
+                  : (this.drugs[i].benefit -= 1);
               }
             }
           } else {
-            this.drugs[i].benefit =
-              this.drugs[i].benefit - this.drugs[i].benefit;
+            this.drugs[i].benefit -= this.drugs[i].benefit;
           }
         } else {
           if (this.drugs[i].benefit < 50) {
-            this.drugs[i].benefit = this.drugs[i].benefit + 1;
+            this.drugs[i].benefit += 1;
           }
         }
       }
--- pharmacy.test.js
+++ pharmacy.test.js
@@ -2,8 +2,65 @@ import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
   it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
+    expect(
+      new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("test", 1, 2)]);
+  });
+
+  it("should decrease benefit twice as fast if expiration has passed", () => {
+    expect(
+      new Pharmacy([new Drug("test", -1, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("test", -2, 1)]);
+  });
+
+  it("should never be a negative item benefit", () => {
+    expect(
+      new Pharmacy([new Drug("test", -1, 0)]).updateBenefitValue()
+    ).toEqual([new Drug("test", -2, 0)]);
+  });
+
+  it("should never be an item benefit over 50", () => {
+    expect(
+      new Pharmacy([new Drug("Herbal Tea", 5, 50)]).updateBenefitValue()
+    ).toEqual([new Drug("Herbal Tea", 4, 50)]);
+  });
+
+  it("should never decrease any value if Magic Pill", () => {
+    expect(
+      new Pharmacy([new Drug("Magic Pill", 2, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("Magic Pill", 2, 3)]);
+  });
+
+  it("should increase benefit if Herbal Tea, twice faster after expiration", () => {
+    expect(
+      new Pharmacy([new Drug("Herbal Tea", 15, 9)]).updateBenefitValue()
+    ).toEqual([new Drug("Herbal Tea", 14, 10)]);
+    expect(
+      new Pharmacy([new Drug("Herbal Tea", -1, 9)]).updateBenefitValue()
+    ).toEqual([new Drug("Herbal Tea", -2, 11)]);
+  });
+
+  it("should dynamically increase or drop to 0 when expired if Fervex", () => {
+    //Benefit increases by 2 when there are 10 days or less
+    expect(
+      new Pharmacy([new Drug("Fervex", 10, 5)]).updateBenefitValue()
+    ).toEqual([new Drug("Fervex", 9, 7)]);
+    //Benefit increases by 3 when there are 5 days or less
+    expect(
+      new Pharmacy([new Drug("Fervex", 5, 5)]).updateBenefitValue()
+    ).toEqual([new Drug("Fervex", 4, 8)]);
+    //Benefit drops to 0 when expired
+    expect(
+      new Pharmacy([new Drug("Fervex", 0, 5)]).updateBenefitValue()
+    ).toEqual([new Drug("Fervex", -1, 0)]);
+  });
+
+  it("should decrease benefit of Dafalgan twice faster than a normal drug", () => {
+    expect(
+      new Pharmacy([new Drug("Dafalgan", 10, 5)]).updateBenefitValue()
+    ).toEqual([new Drug("Dafalgan", 9, 3)]);
+    expect(
+      new Pharmacy([new Drug("Dafalgan", -2, 5)]).updateBenefitValue()
+    ).toEqual([new Drug("Dafalgan", -3, 1)]);
   });
 });
