ea617ff088f4dcad8fcca42e69ce868278cdc2ae
scaraude <ludodante@hotmail.fr>
2021-02-04 16:14:01 +0100

    testing improvement

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,6 +1,10 @@
 import { Drug, HerbalTea, MagicPill, Fervex, Dafalgan, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
+  const pharmacy = new Pharmacy();
+  it("this.drug should be an empty array", () => {
+    expect(pharmacy.drugs).toEqual([])
+  });
   it("should decrease the benefit and expiresIn", () => {
     expect(new Pharmacy([new Drug({name:"test",expiresIn: 2,benefit: 3})]).updateBenefitValue()).toEqual(
       [new Drug({name:"test",expiresIn: 1,benefit: 2})]
@@ -20,52 +24,55 @@ describe("Drug", () => {
     );
   });
   it("this.benefitAging should be equal -1", () => {
-    expect(new Drug({name:"test",expiresIn: 2,benefit: 3}).benefitAging).toEqual(
+    expect(new Drug({expiresIn: 2,benefit: 3}).benefitAging).toEqual(
       -1
     );
   });
   it("benefitAgingWhenExpired should be equal -2;", () => {
-    expect(new Drug({name:"test",expiresIn: 2,benefit: 3}).benefitAgingWhenExpired).toEqual(
+    expect(new Drug({expiresIn: 2,benefit: 3}).benefitAgingWhenExpired).toEqual(
       -2
     );
   });
   it("this.expirationDateAging should be equal -1;", () => {
-    expect(new Drug({name:"test",expiresIn: 2,benefit: 3}).expirationDateAging).toEqual(
+    expect(new Drug({expiresIn: 2,benefit: 3}).expirationDateAging).toEqual(
       -1
     );
   });
-  it("should decrease the benefit by one", () => {
-    expect(new Drug({name:"test",expiresIn: 2,benefit: 3}).updateBenefit(-1)).toEqual(
+
+  const genericDrug = new Drug({expiresIn: 2,benefit: 3})
+  it("updateBenefit() should decrease the benefit by one", () => {
+    expect(genericDrug.updateBenefit(genericDrug.benefitAging)).toEqual(
       2
     );
   });
-  it("should decrease the benefit by two", () => {
-    expect(new Drug({name:"test",expiresIn: -2,benefit: 3}).updateBenefit(-2)).toEqual(
+  it("updateBenefit() should decrease the benefit by two when expired", () => {
+    expect(genericDrug.updateBenefit(genericDrug.benefitAgingWhenExpired)).toEqual(
       1
     );
   });
-  it("should decrease the expiresIn", () => {
-    expect(new Drug({name:"test",expiresIn: 2,benefit: 3}).updateExpiresIn()).toEqual(
+
+  it("updateExpiresIn() should decrease the expiresIn", () => {
+    expect(new Drug({expiresIn: 2,benefit: 3}).updateExpiresIn()).toEqual(
       1
     );
   });
-  it("should validate the benefit for negative value", () => {
-    expect(new Drug({name:"test",expiresIn: 2,benefit: -23}).validateBenefit()).toEqual(
+  it("validateBenefit() should validate the benefit for negative value", () => {
+    expect(new Drug({expiresIn: 2,benefit: -23}).validateBenefit()).toEqual(
       0
     );
   });
-  it("should validate the benefit for positive value", () => {
-    expect(new Drug({name:"test",expiresIn: 2,benefit: 68}).validateBenefit()).toEqual(
+  it("validateBenefit() should validate the benefit for positive value", () => {
+    expect(new Drug({expiresIn: 2,benefit: 68}).validateBenefit()).toEqual(
       50
     );
   });
-  it("should return true", () => {
-    expect(new Drug({name:"test",expiresIn: -2,benefit: 3}).isExpired()).toEqual(
+  it("isExpired() should return true", () => {
+    expect(new Drug({expiresIn: -2,benefit: 3}).isExpired()).toEqual(
       true
     );
   });
-  it("handleAging should decrease the benefit and expiresIn by one", () => {
-    const test = new Drug({name:"test",expiresIn: 2,benefit: 3})
+  it("handleAging() should decrease the benefit and expiresIn by one", () => {
+    const test = new Drug({expiresIn: 2,benefit: 3})
     test.handleAging();
     expect(test.benefit).toEqual(
       2
@@ -74,8 +81,8 @@ describe("Drug", () => {
       1
     );
   });
-  it("handleAging should decrease the benefit by two and expiresIn by one", () => {
-    const test = new Drug({name:"test",expiresIn: -2,benefit: 3})
+  it("handleAging() should decrease the benefit by two and expiresIn by one", () => {
+    const test = new Drug({expiresIn: -2,benefit: 3})
     test.handleAging();
     expect(test.benefit).toEqual(
       1
@@ -87,6 +94,11 @@ describe("Drug", () => {
 });
 
 describe("HerbalTea", () => {
+  it("should be named Herbal Tea", () => {
+    expect(new HerbalTea({expiresIn: 2,benefit: 3}).name).toEqual(
+      "Herbal Tea"
+    );
+  });
   it("this.benefitAging should be equal 1", () => {
     expect(new HerbalTea({expiresIn: 2,benefit: 3}).benefitAging).toEqual(
       1
@@ -102,37 +114,40 @@ describe("HerbalTea", () => {
       -1
     );
   });
-  it("should increase benefit by one", () => {
-    expect(new HerbalTea({expiresIn: 2,benefit: 3}).updateBenefit(1)).toEqual(
+
+  const herbalTea = new HerbalTea({expiresIn: 2,benefit: 3})
+  it("updateBenefit() should increase benefit by one", () => {
+    expect(herbalTea.updateBenefit(herbalTea.benefitAging)).toEqual(
       4
     );
   });
-  it("should increase benefit by two", () => {
-    expect(new HerbalTea({expiresIn: -2,benefit: 3}).updateBenefit(2)).toEqual(
+  it("updateBenefit() should increase benefit by two when expired", () => {
+    expect(herbalTea.updateBenefit(herbalTea.benefitAgingWhenExpired)).toEqual(
       5
     );
   });
-  it("should decrease the expiresIn", () => {
+
+  it("updateExpiresIn() should decrease the expiresIn", () => {
     expect(new HerbalTea({expiresIn: 2,benefit: 3}).updateExpiresIn()).toEqual(
       1
     );
   });
-  it("should validate the benefit for negative value", () => {
+  it("validateBenefit() should validate the benefit for negative value", () => {
     expect(new HerbalTea({expiresIn: 2,benefit: -23}).validateBenefit()).toEqual(
       0
     );
   });
-  it("should validate the benefit for positive value", () => {
+  it("validateBenefit() should validate the benefit for positive value", () => {
     expect(new HerbalTea({expiresIn: 2,benefit: 68}).validateBenefit()).toEqual(
       50
     );
   });
-  it("should return true", () => {
+  it("isExpired() should return true", () => {
     expect(new HerbalTea({expiresIn: -2,benefit: 3}).isExpired()).toEqual(
       true
     );
   });
-  it("handleAging should increase the benefit by one and decrease expiresIn by one", () => {
+  it("handleAging() should increase the benefit by one and decrease expiresIn by one", () => {
     const test = new HerbalTea({expiresIn: 2,benefit: 3})
     test.handleAging();
     expect(test.benefit).toEqual(
@@ -142,7 +157,7 @@ describe("HerbalTea", () => {
       1
     );
   });
-  it("handleAging should increase the benefit by two and decrease expiresIn by one", () => {
+  it("handleAging() should increase the benefit by two and decrease expiresIn by one", () => {
     const test = new HerbalTea({expiresIn: -2,benefit: 3})
     test.handleAging();
     expect(test.benefit).toEqual(
@@ -155,6 +170,11 @@ describe("HerbalTea", () => {
 });
 
 describe("MagicPill", () => {
+  it("should be named Magic Pill", () => {
+    expect(new MagicPill({expiresIn: 2,benefit: 3}).name).toEqual(
+      "Magic Pill"
+    );
+  });
   it("this.benefitAging should be equal 0", () => {
     expect(new MagicPill({expiresIn: 2,benefit: 3}).benefitAging).toEqual(
       0
@@ -170,32 +190,40 @@ describe("MagicPill", () => {
       0
     );
   });
-  it("should not increase benefit", () => {
-    expect(new MagicPill({expiresIn: 2,benefit: 3}).updateBenefit(0)).toEqual(
+
+  const magicPill = new MagicPill({expiresIn: 2,benefit: 3})
+  it("updateBenefit() should not increase benefit", () => {
+    expect(magicPill.updateBenefit(magicPill.benefitAging)).toEqual(
      3
     );
   });
-  it("should not decrease expiresIn", () => {
+  it("updateBenefit() should not increase benefit when expired", () => {
+    expect(magicPill.updateBenefit(magicPill.benefitAgingWhenExpired)).toEqual(
+     3
+    );
+  });
+
+  it("updateExpiresIn() should not decrease expiresIn", () => {
     expect(new MagicPill({expiresIn: 2,benefit: 3}).updateExpiresIn()).toEqual(
       2
     );
   });
-  it("should validate the benefit for negative value", () => {
+  it("validateBenefit() should validate the benefit for negative value", () => {
     expect(new MagicPill({expiresIn: 2,benefit: -23}).validateBenefit()).toEqual(
       0
     );
   });
-  it("should validate the benefit for positive value", () => {
+  it("validateBenefit() should validate the benefit for positive value", () => {
     expect(new MagicPill({expiresIn: 2,benefit: 68}).validateBenefit()).toEqual(
       50
     );
   });
-  it("should return true", () => {
+  it("isExpired() should return true", () => {
     expect(new MagicPill({expiresIn: -2,benefit: 3}).isExpired()).toEqual(
       true
     );
   });
-  it("handleAging should not increase the benefit and expiresIn", () => {
+  it("handleAging() should not increase the benefit and expiresIn", () => {
     const test = new MagicPill({expiresIn: 2,benefit: 3})
     test.handleAging();
     expect(test.benefit).toEqual(
@@ -205,7 +233,7 @@ describe("MagicPill", () => {
       2
     );
   });
-  it("handleAging should not increase the benefit and expiresIn", () => {
+  it("handleAging() should not increase the benefit and expiresIn", () => {
     const test = new MagicPill({expiresIn: -2,benefit: 3})
     test.handleAging();
     expect(test.benefit).toEqual(
@@ -218,6 +246,11 @@ describe("MagicPill", () => {
 });
 
 describe("Fervex", () => {
+  it("should be named Fervex", () => {
+    expect(new Fervex({expiresIn: 2,benefit: 3}).name).toEqual(
+      "Fervex"
+    );
+  });
   it("this.benefitAging should be equal 1", () => {
     expect(new Fervex({expiresIn: 2,benefit: 3}).benefitAging).toEqual(
       1
@@ -233,47 +266,55 @@ describe("Fervex", () => {
       -1
     );
   });
-  it("evaluateBenefitEvolution should return 3", () => {
+  it("evaluateBenefitEvolution() should return 3", () => {
     expect(new Fervex({expiresIn: 2,benefit: 3}).evaluateBenefitEvolution()).toEqual(
      3
     );
   });
-  it("evaluateBenefitEvolution should return 2", () => {
+  it("evaluateBenefitEvolution() should return 2", () => {
     expect(new Fervex({expiresIn: 7,benefit: 3}).evaluateBenefitEvolution()).toEqual(
      2
     );
   });
-  it("evaluateBenefitEvolution should return 1", () => {
+  it("evaluateBenefitEvolution() should return 1", () => {
     expect(new Fervex({expiresIn: 16,benefit: 3}).evaluateBenefitEvolution()).toEqual(
      1
     );
   });
-  it("should increase benefit by three", () => {
-    expect(new Fervex({expiresIn: 2,benefit: 3}).updateBenefit(3)).toEqual(
-     6
+
+  const fervex = new Fervex({expiresIn: 16,benefit: 3})
+  it("updateBenefit() should increase benefit by 1", () => {
+    expect(fervex.updateBenefit(fervex.benefitAging)).toEqual(
+     4
     );
   });
-  it("should decrease expiresIn", () => {
+  it("updateBenefit() should not increase benefit when expired", () => {
+    expect(fervex.updateBenefit(fervex.benefitAgingWhenExpired)).toEqual(
+     3
+    );
+  });
+
+  it("updateExpiresIn() should decrease expiresIn", () => {
     expect(new Fervex({expiresIn: 2,benefit: 3}).updateExpiresIn()).toEqual(
       1
     );
   });
-  it("should validate the benefit for negative value", () => {
+  it("validateBenefit() should validate the benefit for negative value", () => {
     expect(new Fervex({expiresIn: 2,benefit: -23}).validateBenefit()).toEqual(
       0
     );
   });
-  it("should validate the benefit for positive value", () => {
+  it("validateBenefit() should validate the benefit for positive value", () => {
     expect(new Fervex({expiresIn: 2,benefit: 68}).validateBenefit()).toEqual(
       50
     );
   });
-  it("should return true", () => {
+  it("isExpired() should return true", () => {
     expect(new Fervex({expiresIn: -2,benefit: 3}).isExpired()).toEqual(
       true
     );
   });
-  it("handleAging should increase the benefit by 3 and decrease expiresIn", () => {
+  it("handleAging() should increase the benefit by 3 and decrease expiresIn", () => {
     const test = new Fervex({expiresIn: 2,benefit: 3})
     test.handleAging();
     expect(test.benefit).toEqual(
@@ -283,7 +324,7 @@ describe("Fervex", () => {
       1
     );
   });
-  it("handleAging should increase the benefit by 2 and decrease expiresIn", () => {
+  it("handleAging() should increase the benefit by 2 and decrease expiresIn", () => {
     const test = new Fervex({expiresIn: 7,benefit: 3})
     test.handleAging();
     expect(test.benefit).toEqual(
@@ -293,7 +334,7 @@ describe("Fervex", () => {
       6
     );
   });
-  it("handleAging should increase the benefit by 1 and decrease expiresIn", () => {
+  it("handleAging() should increase the benefit by 1 and decrease expiresIn", () => {
     const test = new Fervex({expiresIn: 16,benefit: 3})
     test.handleAging();
     expect(test.benefit).toEqual(
@@ -303,7 +344,7 @@ describe("Fervex", () => {
       15
     );
   });
-  it("handleAging should not increase the benefit and decrease expiresIn", () => {
+  it("handleAging() should not increase the benefit and decrease expiresIn", () => {
     const test = new Fervex({expiresIn: -2,benefit: 3})
     test.handleAging();
     expect(test.benefit).toEqual(
@@ -316,6 +357,11 @@ describe("Fervex", () => {
 });
 
 describe("Dafalgan", () => {
+  it("should be named Dafalgan", () => {
+    expect(new Dafalgan({expiresIn: 2,benefit: 3}).name).toEqual(
+      "Dafalgan"
+    );
+  });
   it("this.benefitAging should be equal -2", () => {
     expect(new Dafalgan({expiresIn: 2,benefit: 3}).benefitAging).toEqual(
       -2
@@ -331,32 +377,40 @@ describe("Dafalgan", () => {
       -1
     );
   });
-  it("should decrese benefit by 2", () => {
-    expect(new Dafalgan({expiresIn: 2,benefit: 3}).updateBenefit(-2)).toEqual(
-     1
+
+  const dafalgan = new Dafalgan({expiresIn: 2,benefit: 10})
+  it("updateBenefit() should decrese benefit by 2", () => {
+    expect(dafalgan.updateBenefit(dafalgan.benefitAging)).toEqual(
+     8
     );
   });
-  it("should decrease expiresIn", () => {
+  it("updateBenefit() should decrese benefit by 4 when expired", () => {
+    expect(dafalgan.updateBenefit(dafalgan.benefitAgingWhenExpired)).toEqual(
+     6
+    );
+  });
+
+  it("updateExpiresIn() should decrease expiresIn", () => {
     expect(new Dafalgan({expiresIn: 2,benefit: 3}).updateExpiresIn()).toEqual(
       1
     );
   });
-  it("should validate the benefit for negative value", () => {
+  it("validateBenefit() should validate the benefit for negative value", () => {
     expect(new Dafalgan({expiresIn: 2,benefit: -23}).validateBenefit()).toEqual(
       0
     );
   });
-  it("should validate the benefit for positive value", () => {
+  it("validateBenefit() should validate the benefit for positive value", () => {
     expect(new Dafalgan({expiresIn: 2,benefit: 68}).validateBenefit()).toEqual(
       50
     );
   });
-  it("should return true", () => {
+  it("isExpired() should return true", () => {
     expect(new Dafalgan({expiresIn: -2,benefit: 3}).isExpired()).toEqual(
       true
     );
   });
-  it("handleAging should decrease the benefit by two and expiresIn by one", () => {
+  it("handleAging() should decrease the benefit by two and expiresIn by one", () => {
     const test = new Dafalgan({expiresIn: 2,benefit: 3})
     test.handleAging();
     expect(test.benefit).toEqual(
@@ -366,7 +420,7 @@ describe("Dafalgan", () => {
       1
     );
   });
-  it("handleAging should decrease the benefit by four and expiresIn by one", () => {
+  it("handleAging() should decrease the benefit by four and expiresIn by one", () => {
     const test = new Dafalgan({expiresIn: -2,benefit: 6})
     test.handleAging();
     expect(test.benefit).toEqual(
