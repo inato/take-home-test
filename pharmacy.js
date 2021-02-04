44dbefbfa164086dce39b8f32cec2566835e0364
scaraude <ludodante@hotmail.fr>
2021-02-04 11:13:35 +0100

    class HerbalTea valided

--- pharmacy.js
+++ pharmacy.js
@@ -51,6 +51,49 @@ export class HerbalTea extends Drug {
   }
 }
 
+export class MagicPill extends Drug {
+  constructor(expiresIn, benefit){
+    super("Magic Pill", expiresIn, benefit);
+    this.benefitAging = 0;
+    this.benefitAgingWhenExpired = this.benefitAging * 2;
+    this.expirationDateAging = 0;
+  }
+
+  handleAging(){
+    return super.handleAging();
+  }
+}
+
+export class Fervex extends Drug {
+  constructor(expiresIn, benefit){
+    super("Fervex", expiresIn, benefit);
+    this.benefitAging = 1;
+    this.benefitAgingWhenExpired = 0;
+  }
+
+  evaluateBenefitEvolution(){
+    const benefitAging = (this.expiresIn <= 5) ? 3 : (this.expiresIn <= 10) ? 2 : this.benefitAging;
+    return benefitAging
+  }
+
+  handleAging(){
+    this.benefitAging = evaluateBenefitEvolution();
+    return super.handleAging();
+  }
+}
+
+export class Dafalgan extends Drug {
+  constructor(expiresIn, benefit){
+    super("Dafalgan", expiresIn, benefit);
+    this.benefitAging = this.benefitAging * 2;
+    this.benefitAgingWhenExpired = this.benefitAging * 2;
+  }
+
+  handleAging(){
+    return super.handleAging();
+  }
+}
+
 export class Pharmacy {
   constructor(drugs = []) {
     this.drugs = drugs;
--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,4 +1,4 @@
-import { Drug, HerbalTea, Pharmacy } from "./pharmacy";
+import { Drug, HerbalTea, MagicPill, Fervex, Dafalgan, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
   it("should decrease the benefit and expiresIn", () => {
@@ -99,6 +99,150 @@ describe("HerbalTea", () => {
   });
   it("should return true", () => {
     expect(new HerbalTea(-2, 3).isExpired()).toEqual(
+      true
+    );
+  });
+});
+
+describe("MagicPill", () => {
+  it("this.benefitAging should be equal 0", () => {
+    expect(new MagicPill(2, 3).benefitAging).toEqual(
+      0
+    );
+  });
+  it("benefitAgingWhenExpired should be equal 0;", () => {
+    expect(new MagicPill(2, 3).benefitAgingWhenExpired).toEqual(
+      0
+    );
+  });
+  it("this.expirationDateAging should be equal 0;", () => {
+    expect(new MagicPill(2, 3).expirationDateAging).toEqual(
+      0
+    );
+  });
+  it("should not increase benefit", () => {
+    expect(new MagicPill(2, 3).updateBenefit(0)).toEqual(
+     3
+    );
+  });
+  it("should not decrease expiresIn", () => {
+    expect(new MagicPill(2, 3).updateExpiresIn()).toEqual(
+      2
+    );
+  });
+  it("should validate the benefit for negative value", () => {
+    expect(new MagicPill(2, -23).validateBenefit()).toEqual(
+      0
+    );
+  });
+  it("should validate the benefit for positive value", () => {
+    expect(new MagicPill(2, 68).validateBenefit()).toEqual(
+      50
+    );
+  });
+  it("should return true", () => {
+    expect(new MagicPill(-2, 3).isExpired()).toEqual(
+      true
+    );
+  });
+});
+
+describe("Fervex", () => {
+  it("this.benefitAging should be equal 1", () => {
+    expect(new Fervex(2, 3).benefitAging).toEqual(
+      0
+    );
+  });
+  it("benefitAgingWhenExpired should be equal 0", () => {
+    expect(new Fervex(2, 3).benefitAgingWhenExpired).toEqual(
+      0
+    );
+  });
+  it("this.expirationDateAging should be equal -1", () => {
+    expect(new Fervex(2, 3).expirationDateAging).toEqual(
+      -1
+    );
+  });
+  it("evaluateBenefitEvolution should return 3", () => {
+    expect(new Fervex(2, 3).evaluateBenefitEvolution()).toEqual(
+     3
+    );
+  });
+  it("evaluateBenefitEvolution should return 2", () => {
+    expect(new Fervex(7, 3).evaluateBenefitEvolution()).toEqual(
+     2
+    );
+  });
+  it("evaluateBenefitEvolution should return 1", () => {
+    expect(new Fervex(16, 3).evaluateBenefitEvolution()).toEqual(
+     1
+    );
+  });
+  it("should increase benefit by three", () => {
+    expect(new Fervex(2, 3).updateBenefit(3)).toEqual(
+     6
+    );
+  });
+  it("should decrease expiresIn", () => {
+    expect(new Fervex(2, 3).updateExpiresIn()).toEqual(
+      1
+    );
+  });
+  it("should validate the benefit for negative value", () => {
+    expect(new Fervex(2, -23).validateBenefit()).toEqual(
+      0
+    );
+  });
+  it("should validate the benefit for positive value", () => {
+    expect(new Fervex(2, 68).validateBenefit()).toEqual(
+      50
+    );
+  });
+  it("should return true", () => {
+    expect(new Fervex(-2, 3).isExpired()).toEqual(
+      true
+    );
+  });
+});
+
+describe("Dafalgan", () => {
+  it("this.benefitAging should be equal -2", () => {
+    expect(new Dafalgan(2, 3).benefitAging).toEqual(
+      -2
+    );
+  });
+  it("benefitAgingWhenExpired should be equal -4", () => {
+    expect(new Dafalgan(2, 3).benefitAgingWhenExpired).toEqual(
+      -4
+    );
+  });
+  it("this.expirationDateAging should be equal -1", () => {
+    expect(new Dafalgan(2, 3).expirationDateAging).toEqual(
+      -1
+    );
+  });
+  it("should decrese benefit by 2", () => {
+    expect(new Dafalgan(2, 3).updateBenefit(-2)).toEqual(
+     1
+    );
+  });
+  it("should decrease expiresIn", () => {
+    expect(new Dafalgan(2, 3).updateExpiresIn()).toEqual(
+      1
+    );
+  });
+  it("should validate the benefit for negative value", () => {
+    expect(new Dafalgan(2, -23).validateBenefit()).toEqual(
+      0
+    );
+  });
+  it("should validate the benefit for positive value", () => {
+    expect(new Dafalgan(2, 68).validateBenefit()).toEqual(
+      50
+    );
+  });
+  it("should return true", () => {
+    expect(new Dafalgan(-2, 3).isExpired()).toEqual(
       true
     );
   });
