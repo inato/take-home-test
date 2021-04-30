cb0ed35640f1ddfe8ab53d77273bf5de61033396
Corentin Hermet <vroosk+gitlab@gmail.com>
2021-05-01 00:39:05 +0200

    refactor: move tests about Fervex to its own file

---
+++ drugs/fervex.test.js
@@ -0,0 +1,32 @@
+import { Fervex } from ".";
+
+describe("Fervex drug", () => {
+  it("should increase fervex benefit by 2 when there are 10 days or less remaining", () => {
+    const expiresIn = 10;
+    const fervex = new Fervex(expiresIn, 0);
+
+    for (let i = expiresIn; i > 5; i--) {
+      fervex.simulateOneDay();
+    }
+
+    expect(fervex.benefit).toEqual(10);
+  });
+
+  it("should increase fervex benefit by 3 when there are 5 days or less remaining", () => {
+    const expiresIn = 5;
+    const fervex = new Fervex(expiresIn, 0);
+
+    for (let i = expiresIn; i > 0; i--) {
+      fervex.simulateOneDay();
+    }
+
+    expect(fervex.benefit).toEqual(15);
+  });
+
+  it("should set fervex benefit to 0 when it expires", () => {
+    const fervex = new Fervex(0, 50);
+    fervex.simulateOneDay();
+
+    expect(fervex.benefit).toEqual(0);
+  });
+});
