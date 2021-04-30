16a004fa43ffb213a3bf98d8c9bceef2c6dad5c7
Corentin Hermet <vroosk+gitlab@gmail.com>
2021-04-30 16:52:08 +0200

    test: add snapshot to pharmacy to ensure generic correctness

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,27 @@
-import { Drug, Pharmacy } from "./pharmacy";
+import { Pharmacy } from "./pharmacy";
+import { Drug } from "./drugs";
 
 describe("Pharmacy", () => {
   it("should decrease the benefit and expiresIn", () => {
     expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
       [new Drug("test", 1, 2)]
     );
+  });
+
+  it("should match snapshot", () => {
+    const drugs = [
+      new Drug("Doliprane", 20, 30),
+      new Drug("Herbal Tea", 10, 5),
+      new Drug("Fervex", 5, 40),
+      new Drug("Magic Pill", 15, 40)
+    ];
+    const pharmacy = new Pharmacy(drugs);
+
+    let result;
+    for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
+      result = pharmacy.updateBenefitValue();
+    }
+
+    expect(result).toMatchSnapshot();
   });
 });
