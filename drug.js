d8e2b7bcb7321e3a183f4081518e908517310952
Jimmy MUNOZ <jmunoz@adilibre.fr>
2021-05-07 15:23:58 +0200

    refactor: remove drugs logic form pharmacy

--- drug.js
+++ drug.js
@@ -9,12 +9,6 @@ export class Drug {
     this.expiresIn -= 1;
   }
 
-  calcExpirationDays() {
-    // Once the expiration date has passed, Benefit degrades twice as fast.
-    const expirationDays = this.expiresIn > 0 ? 1 : 2;
-    return expirationDays;
-  }
-
   applyBenefitRules() {
     // The Benefit of an item is never more than 50.
     if (this.benefit > 50) {
@@ -27,9 +21,10 @@ export class Drug {
   }
 
   calcNewBenefit() {
-    const expirationDays = this.calcExpirationDays();
+    // Once the expiration date has passed, Benefit degrades twice as fast.
+    const expirationFactor = this.expiresIn >= 0 ? 1 : 2;
     // At the end of each day our system lowers both values for every drug
-    this.benefit = this.benefit - expirationDays;
+    this.benefit = this.benefit - expirationFactor;
   }
 
   updateBenefitValue() {
@@ -39,4 +34,35 @@ export class Drug {
   }
 }
 
-export class HerbalTea extends Drug {}
+// "Herbal Tea" actually increases in Benefit the older it gets.
+// Benefit increases twice as fast after the expiration date.
+export class HerbalTea extends Drug {
+  calcNewBenefit() {
+    const expirationFactor = this.expiresIn < 0 ? 2 : 1;
+    this.benefit = this.benefit + expirationFactor;
+  }
+}
+
+// "Magic Pill" never expires nor decreases in Benefit.
+export class MagicPill extends Drug {
+  expiresOneDay() {}
+  calcNewBenefit() {}
+}
+
+// "Fervex", like Herbal Tea, increases in Benefit as its expiration date approaches.
+// Benefit increases by 2 when there are 10 days or less
+// and by 3 when there are 5 days or less but Benefit drops to 0 after the expiration date.
+export class Fervex extends Drug {
+  expiresOneDay() {}
+  calcNewBenefit() {
+    if (this.expiresIn <= 0) {
+      this.benefit = 0;
+    } else if (this.expiresIn <= 5) {
+      this.benefit = this.benefit + 3;
+    } else if (this.expiresIn <= 10) {
+      this.benefit = this.benefit + 2;
+    } else {
+      this.benefit = this.benefit + 1;
+    }
+  }
+}
--- drug.test.js
+++ drug.test.js
@@ -1,4 +1,4 @@
-import { Drug } from "./drug";
+import { Drug, HerbalTea, MagicPill, Fervex } from "./drug";
 
 describe("Drug", () => {
   it("At the end of each day our system lowers both values for every drug", () => {
@@ -27,5 +27,62 @@ describe("Drug", () => {
     testDrug.updateBenefitValue();
     // Assert
     expect(testDrug.benefit).toBe(2);
+  });
+
+  it(`"Herbal Tea" actually increases in Benefit the older it gets. Benefit increases twice as fast after the expiration date.`, () => {
+    // Arrange
+    const testDrug = new HerbalTea("Herbal Tea", 0, 4);
+    // Act
+    testDrug.updateBenefitValue();
+    // Assert
+    expect(testDrug.benefit).toBe(6);
+  });
+
+  it(`The Benefit of an item is never negative`, () => {
+    // Arrange
+    const testDrug = new HerbalTea("Herbal Tea", 0, 50);
+    // Act
+    testDrug.updateBenefitValue();
+    // Assert
+    expect(testDrug.benefit).toBe(50);
+  });
+
+  it(`"Magic Pill" never expires nor decreases in Benefit.`, () => {
+    // Arrange
+    const testDrug = new MagicPill("Magic Pill", 5, 5);
+    // Act
+    testDrug.updateBenefitValue();
+    // Assert
+    expect(testDrug.benefit).toBe(5);
+    expect(testDrug.expiresIn).toBe(5);
+  });
+
+  describe("Fervex", () => {
+    it(`Benefit increases by 2 when there are 10 days or less`, () => {
+      // Arrange
+      const testDrug = new Fervex("Fervex", 9, 5);
+      // Act
+      testDrug.updateBenefitValue();
+      // Assert
+      expect(testDrug.benefit).toBe(7);
+    });
+
+    it(`Benefit increases by 3 when there are 5 days or less`, () => {
+      // Arrange
+      const testDrug = new Fervex("Fervex", 5, 5);
+      // Act
+      testDrug.updateBenefitValue();
+      // Assert
+      expect(testDrug.benefit).toBe(8);
+    });
+
+    it(`Benefit drops to 0 after the expiration date.`, () => {
+      // Arrange
+      const testDrug = new Fervex("Fervex", 0, 5);
+      // Act
+      testDrug.updateBenefitValue();
+      // Assert
+      expect(testDrug.benefit).toBe(0);
+    });
   });
 });
