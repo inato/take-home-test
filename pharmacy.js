a64349708c16a02bec9db8887cdfba9e10d68786
jcazako <jeancharles.cazako@gmail.com>
2021-11-08 12:52:41 +0100

    handle fervex

--- pharmacy.js
+++ pharmacy.js
@@ -72,6 +72,7 @@ export class Pharmacy {
 
     // calculate what the value of benefit would be if we don't apply limitations
     benefit = benefit + benefitDiff;
+
     // benefit can't be negative nor greater than 50
     benefit = benefit < 0 ? 0 : benefit > 50 ? 50 : benefit;
     return { name, expiresIn, benefit };
@@ -95,14 +96,36 @@ export class Pharmacy {
         expireCoefficient = 0;
       }
 
+      if (name === "Fervex") {
+        if (expiresIn > 10) {
+          benefitCoefficient = 1;
+        }
+        if (expiresIn <= 10 && expiresIn > 5) {
+          benefitCoefficient = 2;
+        }
+        if (expiresIn <= 5 && expiresIn > 0) {
+          benefitCoefficient = 3;
+        }
+      }
+
       // Once the expiration date has passed, Benefit degrades twice as fast.
-      if (expiresIn < 0)
+      if (expiresIn <= 0) {
+        // exception for Fervex that drop to zero
+        if (name === "Fervex") {
+          return this.updateBenefitSafely(
+            drug,
+            expireCoefficient,
+            -1 * benefit
+          );
+        }
         return this.updateBenefitSafely(
           drug,
           expireCoefficient,
           benefitCoefficient * 2
         );
-      // If the expiration date hasn't passed, Benefit degrades twice as fast.
+      }
+
+      // If the expiration date hasn't passed, Benefit degrades by 1.
       return this.updateBenefitSafely(
         drug,
         expireCoefficient,
