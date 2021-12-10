cb813a685b3cb041dd80725b8af1300e88742436
Charles BARDIN <contact@charlesbardin.com>
2021-12-10 18:58:35 +0100

    test: update Pharmacy tests

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,10 +1,36 @@
 import { Pharmacy } from "./pharmacy";
 import { Drug } from "./drug";
 
+let drugs;
+let pharmacy;
 describe("Pharmacy", () => {
-  it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)]
-    );
+  describe("updateBenefitValue", () => {
+    beforeEach(() => {
+      drugs = [
+        new Drug("Doliprane", 20, 30),
+        new Drug("Herbal Tea", 10, 5),
+        new Drug("Fervex", 5, 40),
+        new Drug("Magic Pill", 15, 40),
+      ];
+
+      pharmacy = new Pharmacy(drugs);
+    });
+
+    it("Should return an array of drugs", () => {
+      const result = pharmacy.updateBenefitValue();
+
+      expect(result).toBeInstanceOf(Array);
+      result.forEach((drug) => expect(drug).toBeInstanceOf(Drug));
+    });
+
+    it("should call the updateBenefitValueAndExpiration method of each drug", () => {
+      const spies = drugs.map((drug) =>
+        jest.spyOn(drug, "updateBenefitValueAndExpiration")
+      );
+
+      pharmacy.updateBenefitValue();
+
+      spies.forEach((spy) => expect(spy).toHaveBeenCalledTimes(1));
+    });
   });
 });
