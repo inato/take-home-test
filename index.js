8fe02d9e6b9de9d9da60ca474f82d768430b825f
Charles BARDIN <contact@charlesbardin.com>
2021-12-10 13:18:13 +0100

    feat: make output.txt file more readable
    Adds end of line between each daily output

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-import { Drug, Pharmacy } from "./pharmacy";
-
+import { EOL } from "os";
 import fs from "fs";
+
+import { Drug, Pharmacy } from "./pharmacy";
 
 const drugs = [
   new Drug("Doliprane", 20, 30),
   new Drug("Herbal Tea", 10, 5),
   new Drug("Fervex", 5, 40),
-  new Drug("Magic Pill", 15, 40)
+  new Drug("Magic Pill", 15, 40),
 ];
 const trial = new Pharmacy(drugs);
 
@@ -17,7 +18,7 @@ for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
 }
 
 /* eslint-disable no-console */
-fs.writeFile("output.txt", log, err => {
+fs.writeFile("output.txt", log.join(`,${EOL}`), (err) => {
   if (err) {
     console.log("error");
   } else {
