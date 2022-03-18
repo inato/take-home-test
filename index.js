aa1d73a15d1535c3bd00a7028dba43fc8c9bbf26
chafikgharbi <chafikgharbi1@gmail.com>
2022-03-18 16:18:17 +0100

    Add Dafalgan to drugs

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const drugs = [
   new Drug("Doliprane", 20, 30),
   new Drug("Herbal Tea", 10, 5),
   new Drug("Fervex", 5, 40),
-  new Drug("Magic Pill", 15, 40)
+  new Drug("Magic Pill", 15, 40),
+  new Drug("Dafalgan", 15, 40),
 ];
 const trial = new Pharmacy(drugs);
 
@@ -16,7 +17,7 @@ for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
 }
 
 /* eslint-disable no-console */
-fs.writeFile("output.txt", log.toString(), err => {
+fs.writeFile("output.txt", log.toString(), (err) => {
   if (err) {
     console.log("error");
   } else {
