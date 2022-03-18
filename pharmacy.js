6ee9091b42cec70c19a2f62a7f09888be94103e7
adel djidjik <ea_djidjik@esi.dz>
2022-03-18 16:13:59 +0100

    Refactoring 2 | refactored to be more open for new features and close for modification - stable api-

--- pharmacy.js
+++ pharmacy.js
@@ -1,65 +1,75 @@
-const MAX_BENEFIT= 50;
+const MAX_BENEFIT = 50;
 
+const DrugsRules = {
+  default: (expiresIn_, benefit_) => {
+    let benefit = benefit_;
+    let expiresIn = expiresIn_;
+
+    if (benefit > 0) {
+      benefit -= 1;
+    }
+    expiresIn -= 1;
+    if (expiresIn < 0 && benefit > 0) {
+      benefit -= 1;
+    }
+    return { expiresIn, benefit };
+  },
+  "Herbal Tea": (expiresIn_, benefit_) => {
+    let benefit = benefit_;
+    let expiresIn = expiresIn_;
+    if (benefit < MAX_BENEFIT) {
+      benefit += 1;
+    }
+    expiresIn -= 1;
+
+    if (expiresIn < 0 && benefit < MAX_BENEFIT) {
+      benefit += 1;
+    }
+    return { expiresIn, benefit };
+  },
+  "Magic Pill": (expiresIn_, benefit_) => {
+    return { expiresIn: expiresIn_, benefit: benefit_ };
+  },
+  Fervex: (expiresIn_, benefit_) => {
+    let benefit = benefit_;
+    let expiresIn = expiresIn_;
+    if (benefit < MAX_BENEFIT) {
+      benefit += 1;
+      if (expiresIn < 11) {
+        benefit += 1;
+      }
+      if (expiresIn < 6) {
+        benefit += 1;
+      }
+    }
+    expiresIn -= 1;
+
+    if (expiresIn < 0) {
+      benefit = 0;
+    }
+
+    return { expiresIn, benefit };
+  },
+};
 export class Drug {
   constructor(name, expiresIn, benefit) {
     this.name = name;
     this.expiresIn = expiresIn;
     this.benefit = benefit;
   }
-  isBenefitEditable(){
-    return this.benefit < MAX_BENEFIT
-  }
-  increaseBenefit(){
-    if (this.isBenefitEditable()) {
-      this.benefit = this.benefit + 1;
-    }
-  }
-  updateBenefitValue(){
-    if ( // default Benefit degrades
-      this.name != "Herbal Tea" &&
-      this.name != "Fervex"
-    ) {
-      if (this.benefit > 0) { // The Benefit of an item is never negative
-        if (this.name != "Magic Pill") { // "Magic Pill" never decreases in Benefit
-          this.benefit = this.benefit - 1;
-        }
-      }
-    } else { // "Herbal Tea" || "Fervex"
-      if (this.isBenefitEditable()){ // The Benefit of an item is never more than 50
-        this.benefit = this.benefit + 1;
-        if (this.name == "Fervex") { // "Fervex" custom case
-          if (this.expiresIn < 11) {
-            this.increaseBenefit();
-          }
-          if (this.expiresIn < 6) {
-            this.increaseBenefit();
-          }
-        }
-      }
+
+  updateBenefitValue() {
+    let calculFunction; // the function that will treat benefit and expiresIn depending on the Drug name
+    /* if an explicit rule already declared fo the drug we use it else we use the default method */
+    if (Object.keys(DrugsRules).includes(this.name)) {
+      calculFunction = DrugsRules[this.name];
+    } else {
+      calculFunction = DrugsRules["default"];
     }
 
-    // expiration go down by one day
-    if (this.name != "Magic Pill") { //for this case : "Magic Pill" never expires, default expiresIn -= 1
-      this.expiresIn = this.expiresIn - 1;
-    }
-
-    // verify expiration
-    if (this.expiresIn < 0) { // if drug expired
-      if (this.name != "Herbal Tea") { 
-        if (this.name != "Fervex") {
-          if (this.benefit > 0) {
-            if (this.name != "Magic Pill") { // "Magic Pill" never decreases in Benefit. else decrease benefit when expired
-              this.benefit = this.benefit - 1;
-            }
-          }
-        } else { // "Fervex":
-          this.benefit = 0;
-        }
-      } else {
-        // "Herbal Tea" actually increases in Benefit the older it gets
-        this.increaseBenefit();
-      }
-    }
+    const { benefit, expiresIn } = calculFunction(this.expiresIn, this.benefit); // calculate the new benefit, expiresIn values
+    this.benefit = benefit;
+    this.expiresIn = expiresIn;
   }
 }
 
@@ -69,7 +79,7 @@ export class Pharmacy {
   }
   updateBenefitValue() {
     for (var i = 0; i < this.drugs.length; i++) {
-      this.drugs[i].updateBenefitValue()
+      this.drugs[i].updateBenefitValue();
     }
 
     return this.drugs;
