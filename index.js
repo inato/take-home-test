990fe4e9a41a0599bedfda25e036cc7b15cd33a2
Laure Retru-Chavastel <laure.retru-chavastel@gadz.org>
2022-06-22 11:02:36 +0200

    ♻️ Transform output file to json

--- index.js
+++ index.js
@@ -13,15 +13,20 @@ const trial = new Pharmacy(drugs);
 const log = [];
 
 for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
-  log.push(JSON.stringify(trial.updateBenefitValue()));
+  log.push(JSON.parse(JSON.stringify(trial.updateBenefitValue())));
 }
 
 /* eslint-disable no-console */
-fs.writeFile("output.txt", log.join(","), err => {
-  if (err) {
-    console.log("error");
-  } else {
-    console.log("success");
+fs.writeFile(
+  "output.json",
+  JSON.stringify({ result: log }, null, 2).concat("\n"),
+  err => {
+    if (err) {
+      console.log("error");
+    } else {
+      console.log("success");
+    }
   }
-});
+);
+
 /* eslint-enable no-console */
